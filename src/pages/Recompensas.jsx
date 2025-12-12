4a97b2b69f91f79b7eb76d6d797b9a5426e4583d
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2025-12-12 11:31:16 +0100

    añadir recompensas y dvh para el movil

--- src/pages/Recompensas.jsx
+++ src/pages/Recompensas.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChevronDown, BadgeJapaneseYen } from "lucide-react";
 import GoBack from "../components/GoBack";
 import { ProgressBar } from "../components/ProgressBar";
 import "./Recompensas.css"
 
 export default function Recompensas() {
+
+      const generarNumeros = () => {
+    return Array.from({ length: 4 }, () =>
+      Math.floor(Math.random() * (1000 - 500 + 1)) + 500
+    );
+  };
+
+    const [numeros, setNumeros] = useState(generarNumeros);
+    const money=500;
     const items = [1, 2, 3, 4];
+    const recompensas = ["Bono de 1 mes de transporte público gratis."," 20 € es Steam.","3 meses gratis de Spotify premium.","Descuentos de navidad en mercados locales."]
+
+
+    useEffect(() => {
+
+    setNumeros(generarNumeros());
+  }, []); 
 
     return (
         <div className="rewards">
@@ -14,7 +30,7 @@ export default function Recompensas() {
             <h1 className="rewards__title">Recompensas</h1>
 
             <div className="rewards__balance">
-                <span className="rewards__balance-value">500</span>
+                <span className="rewards__balance-value">{money}</span>
                 <BadgeJapaneseYen size={20} color="goldenrod" />
             </div>
 
@@ -27,15 +43,15 @@ export default function Recompensas() {
                 {items.map((_, i) => (
                     <div key={i} className="reward-card">
                         <div className="reward-card__header">
-                            <span className="reward-card__title">Lorem lorem lorem</span>
+                            <span className="reward-card__title">{recompensas[i]}</span>
 
                             <div className="reward-card__currency">
-                                <span>1000</span>
+                                <span>{numeros[i]}</span>
                                 <BadgeJapaneseYen size={18} color="goldenrod" />
                             </div>
                         </div>
 
-                        <ProgressBar prc={Math.random() * 96} className="reward-card__progress" />
+                        <ProgressBar prc={(numeros[i]-money)*0.1} className="reward-card__progress" />
                     </div>
                 ))}
             </div>
